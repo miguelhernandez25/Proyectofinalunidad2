4bd563a7d45de4107c39ffd24dd5985175ed97eb
miguelhernandez25 <mhernandez111214@gmail.com>
2022-03-22 20:24:53 -0500

    proyectofinalunidad2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function nombre() {
+    alert("nombre completo")
+}
+
+$(".evento1").click( function(){
+    $(".evento1").css("font-size", "100px")
+
+})
+
+$(".evento2").dblclick( function(){
+    $(".evento2").css("font-size", "10px")
+})
+
+$(".evento7").mousemove( function(){
+    $(".evento7").css("font-size", "70px")
+
+})
+
+$(".evento8").mouseleave( function(){
+    $(".evento8").css("font-size", "15px")
+
+})
